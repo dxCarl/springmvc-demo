8faf0ba01c356f0d8f16949743e5aa9d2ee25335
yucong <2241268051@qq.com>
2017-08-11 10:08:57 +0800

    文件上传使用

--- springmvc_03_Restful/src/main/java/com/springmvc/demo/util/Constant.java
+++
@@ -1,9 +0,0 @@
-package com.springmvc.demo.util;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
-public interface Constant {
-
-	Log MY_LOG = LogFactory.getLog(Constant.class);
-}
