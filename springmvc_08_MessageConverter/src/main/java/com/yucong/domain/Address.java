145a255132c13f36782512ff6ef0a72b4faeebbc
email <2241268051@qq.com>
2017-09-27 23:30:30 +0800

    添加springmvc的MessageConter的使用示例

---
+++ springmvc_08_MessageConverter/src/main/java/com/yucong/domain/Address.java
@@ -0,0 +1,13 @@
+package com.yucong.domain;
+
+public class Address {
+	private String tel;
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+}
